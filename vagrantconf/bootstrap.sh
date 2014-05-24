6eae0fdfc2cde91a29ca02ea785c7ede56862a38
Victor Häggqvist <victor@snilius.com>
2014-05-24 14:03:14 +0200

    woop

--- vagrantconf/bootstrap.sh
+++ vagrantconf/bootstrap.sh
@@ -16,9 +16,13 @@ sudo echo "export APACHE_RUN_GROUP=vagrant" >> /etc/apache2/envvars
 
 sudo a2enmod rewrite
 
+# bind mysql to all
+cat /etc/mysql/my.cnf | sed 's/bind-address/#bind-address/' > mymod.cnf
+sudo cp mymod.cnf /etc/mysql/my.cnf
+rm mymod.cnf
+
 # bootstrap mysql
-sudo ln -s /vagrant/vagrantconf/mysql.cnf /etc/mysql/conf.d/mysql-vagrant.cnf
-mysql -u root -proot -e /vagrant/vagrantconf/bootstrap.sql
+mysql -u root -proot -e "source /vagrant/vagrantconf/bootstrap.sql"
 
 # modx
 
@@ -26,18 +30,22 @@ mysql -u root -proot -e /vagrant/vagrantconf/bootstrap.sql
 #wget http://modx.com/download/direct/modx-2.2.14-pl.zip
 
 # from aws
-wget http://modx.s3.amazonaws.com/releases/2.2.14/modx-2.2.14-pl.zip
+echo "Getting MODx..."
+wget -q http://modx.s3.amazonaws.com/releases/2.2.14/modx-2.2.14-pl.zip
 
 # unzip and put to place
 unzip -q modx-2.2.14-pl.zip
 cd modx*
 cp -r * /vagrant/public
-cd
+cd ~
 
 # set php timezone
-wget -O tzupdate.zip https://github.com/cdown/tzupdate/archive/master.zip
+echo "Configuring PHP timezone..."
+wget -q -O tzupdate.zip https://github.com/cdown/tzupdate/archive/master.zip
 unzip -q tzupdate.zip
 sudo echo "date.timezone=\"$(./tzupdate-master/tzupdate -p)\"" >> /etc/php5/apache2/php.ini
+sudo echo "display_errors = On" >> /etc/php5/apache2/php.ini
 
-# restart apache once for all
+# restart once for all
 sudo service apache2 restart
+sudo service mysql restart
